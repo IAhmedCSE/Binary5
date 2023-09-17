300c31281b7263e1f9de285efc0dce01a3344169
IsrakAhmed <israkahmed7@gmail.com>
2023-09-18 00:55:27 +0600

    Password Reset, OTP Sender, OTP Checker - added

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.get("/signup",(req,res) => {
     res.sendFile(__dirname + "/views/signup.html");
 });
 
+app.get("/verify-ID-OTP",(req,res) => {
+    res.sendFile(__dirname + "/views/verify-ID-OTP.html");
+});
+
 app.post("/signup",createUser);
 
 app.get("/:id([0-9]{10})", getOneClassMate);
